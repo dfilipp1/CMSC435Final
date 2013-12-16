f5f75e5ca525368657da0828c2e8e1e8d3e53f48
Aces7824 <sothie1@umbc.edu>
2013-12-16 07:16:15 -0500

    Fully incorporate GUI, add controls, add coloring, add cluster distant, LOOK FREAKING AWESOME

--- FinalProject.cpp
+++ FinalProject.cpp
@@ -1,4 +1,4 @@
-//Created by: Zachary Garbarino
+//Created by: Zachary Garbarino, Dustin Filippi Sothiara Em
 //Due on: 12/16/2013
 //Demo on: 12/10/2013
 
@@ -6,6 +6,9 @@
 #include <stdlib.h>
 #include <string.h>
 #include <GL/glui.h>
+#include <iostream>
+#include <sstream>
+#include <fstream>
 
 #ifdef __APPLE__
   #include <GLUT/glut.h>
@@ -18,106 +21,166 @@
 #include <iostream>
 
 #include "CLUSTER_STRUCT.hpp"
+#include "bundling.hpp"
 
+#define SCALE 40.0
 
 vector<POINT> *points;
+vector<CLUSTER> *cluster_ref;
 int clusters;
 GLint mouseButton = -1;
-char * filename;
 int main_window;
-int scaleunit = 1;
+int scaleunit = 0;
+
+int draw_center = 0;
+int toggle_color = 1;
+
+float con_val[74][74];
+
+GLfloat lowerbound = 0.0;
+GLfloat upperbound = 1.0;
+
+char* filename;
+char* matrix_file;
 
 double * getNewDistant(POINT *p);
 double * getRGB(int hue, double sat, double val);
+void assignColorToCluster();
+
+GLUI_Spinner *upper_spinner;
+GLUI_Spinner *lower_spinner;
+
+CLUSTER_POINT * cluster_point;
 
 void init(){
   //Getting the clustered points
   glClearColor(0,0,0,0);
   tbInit(GLUT_LEFT_BUTTON);
-  points = load_points(filename, clusters);
+  cluster_point = load_points(filename, clusters);
+  points = cluster_point->points;
+  cluster_ref = cluster_point->clusters;
+  assignColorToCluster();
+
+  //Getting the value from the matrix tables
+  ifstream file;
+  file.open(matrix_file);
+  string line;
+  double *value;
   
+  int row = 0;
+  while (getline(file,line)){
+    vector<string> elems;
+    split(line,' ',elems);
+    for (int i = 0;i < 74; i++){
+      con_val[row][i] = atof(elems[i].c_str());
+    }
+    row++;
+  }
+
+  file.close();
 }
 
 void display(void){
   glClear(GL_COLOR_BUFFER_BIT);
-
+  
   glPushMatrix();
   tbMatrix();
   //Drawing the points
   glPointSize(5);
- 
   for (int i = 0; i < points->size(); i++){
     glBegin(GL_POINTS);
     double * coordinates = (*points)[i].coor;
-
-    double *rgb = getRGB(360/clusters * (*points)[i].cluster->ref,1,1);
+    
+    double *rgb = getRGB((*points)[i].cluster->color[0],1,1);
     glColor3f(rgb[0],rgb[1],rgb[2]);
     free(rgb);
-
+    
     double * center = getNewDistant(&(*points)[i]);
-    //glVertex3f(coordinates[0],coordinates[1],coordinates[2]);
     glVertex3f(center[0]/40.0,center[1]/40.0,center[2]/40.0);
     glEnd();
-    
+   
   }
+  glEnd();
   
- 
+  double centers[clusters][4];
+  
+  for (std::vector<POINT>::iterator it=points->begin(); it<points->end(); it++) {
+    centers[(*it).cluster->ref][0]+=(*it).coor[0];
+    centers[(*it).cluster->ref][1]+=(*it).coor[1];
+    centers[(*it).cluster->ref][2]+=(*it).coor[2];
+    centers[(*it).cluster->ref][3]+=1;
+  }
+  for (int cluster = 0; cluster<clusters; cluster++) {
+    centers[cluster][0]/=centers[cluster][3];
+    centers[cluster][1]/=centers[cluster][3];
+    centers[cluster][2]/=centers[cluster][3];
+  }//Find the center of each cluster
+  
+  int this_point_count = 0;
+  int other_point_count = 0;
+  
+  GLfloat Ctrl[4][3];
+  for (std::vector<POINT>::iterator this_cluster=points->begin(); this_cluster<points->end(); this_cluster++) {
+    for (std::vector<POINT>::iterator other_cluster=points->begin(); other_cluster<points->end(); other_cluster++) {
+      if ((*this_cluster).cluster->ref == (*other_cluster).cluster->ref) {
+	other_point_count++;
+	continue;
+      }
+      if(con_val[this_point_count][other_point_count] >= lowerbound && con_val[this_point_count][other_point_count] <= upperbound){
+	double * this_center = getNewDistant(&(*this_cluster));
+	double * other_center = getNewDistant(&(*other_cluster));
+
+	Ctrl[0][0] = this_center[0];
+	Ctrl[0][1] = this_center[1];
+	Ctrl[0][2] = this_center[2];
+
+	Ctrl[3][0] = other_center[0];
+	Ctrl[3][1] = other_center[1];
+	Ctrl[3][2] = other_center[2];
+      
+	Ctrl[1][0] = centers[(*this_cluster).cluster->ref][0];
+	Ctrl[1][1] = centers[(*this_cluster).cluster->ref][1];
+	Ctrl[1][2] = centers[(*this_cluster).cluster->ref][2];
+	Ctrl[2][0] = centers[(*other_cluster).cluster->ref][0];
+	Ctrl[2][1] = centers[(*other_cluster).cluster->ref][1];
+	Ctrl[2][2] = centers[(*other_cluster).cluster->ref][2];
+
+	if (toggle_color == 1){
+	  draw_curve(Ctrl,(&(*this_cluster)),(&(*other_cluster)));
+	}
+	free(this_center);
+	free(other_center);
+      }
+      other_point_count++;
+    }
+    other_point_count = 0;
+    this_point_count++;
+  }//Compute the control points
+  
+  if (draw_center == 1){
+      
+    for(int this_center = 0; this_center<clusters; this_center++){
+      for(int next_center = this_center+1; next_center<clusters; next_center++){
+	glColor3f(1.0, 1.0, 1.0);
+	glBegin(GL_LINES);
+	glVertex3f(centers[this_center][0]/SCALE,centers[this_center][1]/SCALE, centers[this_center][2]/SCALE);
+	glVertex3f(centers[next_center][0]/SCALE,centers[next_center][1]/SCALE, centers[next_center][2]/SCALE);
+	glVertex3f(15, 0, 0);
+	glEnd();
+      }
+    }
+  
+    for(std::vector<POINT>::iterator this_point=points->begin(); this_point<points->end(); this_point++){
+      glColor3f(1.0, 1.0, 1.0);
+      glBegin(GL_LINES);
+      glVertex3f((*this_point).coor[0]/SCALE,(*this_point).coor[1]/SCALE,(*this_point).coor[2]/SCALE);
+      glVertex3f(centers[(*this_point).cluster->ref][0]/SCALE,centers[(*this_point).cluster->ref][1]/SCALE,centers[(*this_point).cluster->ref][2]/SCALE);
+      glEnd();
+    }
+  }
   glPopMatrix();
-  
+   
   glutSwapBuffers();
-}
-
-//Get the RGB values each with a range of 0-1. Passing in the hue sat and val
-double * getRGB(int hue, double sat, double val){
- 
-  GLfloat h = fmod(hue/60.0,2);
-  GLfloat c = (sat) * (val);
-  
-  GLfloat x = c * (1 - fabs(h-1));
-  GLfloat m = sat - c;
-
-  GLfloat R = c+m;
-  GLfloat G = x+m;
-  GLfloat B = m;
-  
-  if (0 <= hue && hue < 60){
-    R = c+m;
-    B = x+m;
-    G = m;
-  }
-  else if (60 <= hue && hue < 120){
-    R = x+m;
-    G = c+m;
-    B = m;
-  }
-  else if (120 <= hue && hue < 180){
-    R = m;
-    G = c+m;
-    B = x+m;
-  }
-  else if (180 <= hue && hue < 240){
-    R = m;
-    G = x+m;
-    B = c+m;
-  }
-  else if (240 <= hue && hue < 300){
-    R = x+m;
-    G = m;
-    B = c+m;
-  }
-  else if (300 <= hue && hue < 360){
-    R = c+m;
-    G = m;
-    B = x+m;
-  }
-
-  //Store the value in a return array
-  double* returnC = new double[3];
-  returnC[0] = R;
-  returnC[1] = G;
-  returnC[2] = B;
-  //Return the color array
-  return returnC;
 }
 
 double * getNewDistant(POINT *p){
@@ -137,7 +200,6 @@ double * getNewDistant(POINT *p){
 
 void reshape(int w, int h){
   tbReshape(w, h);
-  cout<<"Reshape"<<endl;
   glViewport(0, 0, w, h);
   glMatrixMode(GL_PROJECTION);
   glLoadIdentity();
@@ -164,6 +226,17 @@ void keyboard(unsigned char key, int x, int y){
   case 27:
     exit(0);
     break;
+  case 'c':
+    if (draw_center == -1)
+      draw_center = 1;
+    else
+      draw_center = 0;
+    break;
+  case 'v':
+    if (toggle_color == 1)
+      toggle_color = 0;
+    else
+      toggle_color = 1;
   default:
     break;
   }
@@ -181,25 +254,45 @@ void idlefunction(void){
 }
 
 void control_callback(int control){
-  if (control == 100)
-    points = load_points(filename, clusters);
+  if (control == 100){
+    free(cluster_point->points);
+    free(cluster_point->clusters);
+    free(cluster_point);
+    cluster_point= load_points(filename, clusters);
+    points = cluster_point->points;
+    cluster_ref = cluster_point->clusters;
+    assignColorToCluster();
+  }
+  else if (control == 400)
+    lower_spinner->set_float_limits(0.0,upperbound,GLUI_LIMIT_CLAMP);
+  else if (control == 500)
+    upper_spinner->set_float_limits(lowerbound,1.0,GLUI_LIMIT_CLAMP);
   glutPostRedisplay();
 }
 
+void assignColorToCluster(){
+  vector<CLUSTER> * clust = cluster_point->clusters;
+  for (int i = 0; i <clusters;i++){
+    double *color = new double[3];
+    color[0] = (360/clusters) * i;
+    color[1] = 1;
+    color[2] = 2;
+    (*clust)[i].color = color;
+  }
+}
 
 int main(int argc, char *argv[]){
-  //Local Variables	
-
   //Make sure that there is a command-line argument
-  if (argc < 2) {
-    std::cout << "usage: FinalProject <datafile> <number of clusters>" << std::endl;
+  if (argc < 3) {
+    std::cout << "usage: FinalProject <datafile> <patient matrix>" << std::endl;
     exit(-1);
   }
   else {
     //The first command-line arg is the name of the file to use
     filename = argv[1];
-    //num_of_clusters = atoi(argv[2]);
-    clusters = 2; //Just for debugging, the acutal number of clusters is determine after file read
+    matrix_file = argv[2];
+    //Initial number of cluser
+    clusters = 2; 
   }
   
   glutInit(&argc, argv);
@@ -232,8 +325,20 @@ int main(int argc, char *argv[]){
   spinner->set_speed(0);
 
   GLUI_Spinner *scale_unit = new GLUI_Spinner(control,"Seperate", GLUI_SPINNER_INT,&scaleunit,200,control_callback);
-  scale_unit->set_int_limits(1,15,GLUI_LIMIT_CLAMP);
+  scale_unit->set_int_limits(0,15,GLUI_LIMIT_CLAMP);
   scale_unit->set_speed(0);
+
+  upper_spinner = new GLUI_Spinner(control,"Upper bound strength", GLUI_SPINNER_FLOAT,&upperbound,400,control_callback);
+  upper_spinner->set_float_limits(lowerbound,1.0,GLUI_LIMIT_CLAMP);
+  upper_spinner->set_speed(2);
+  
+  lower_spinner = new GLUI_Spinner(control,"Lower bound strength", GLUI_SPINNER_FLOAT,&lowerbound,500,control_callback);
+  lower_spinner->set_float_limits(0.0,upperbound,GLUI_LIMIT_CLAMP);
+  lower_spinner->set_speed(2);
+  
+  //Adding checkboxs for toggling colors
+  glui->add_checkbox_to_panel(control,"Bundle Color",&toggle_color,600,control_callback);
+  glui->add_checkbox_to_panel(control,"Cluster center",&draw_center,700,control_callback);
 
   ///////////////////////////////////////////////////////////
   glutMainLoop();
